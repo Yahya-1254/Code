a628f2067900068bc4407f9e8d2189a3f2a88f25
Yahya-1254 <79520504+Yahya-1254@users.noreply.github.com>
2021-03-01 19:38:22 +0500

    Add files via upload

---
+++ Currency.py
@@ -0,0 +1,3 @@
+d = {"Dollar":158.35, "Euro":191.9,"Japanese Yen":1.49,"Pound":220.61}
+x = input('Enter the currency you want to change into PKR')
+print (d[x])
---
+++ SI Units.py
@@ -0,0 +1,11 @@
+a = {'Mass':'Kilogram','Time':'Seconds','Amount of substance':'mole',
+     'Length':'meter','Tempreatur':'Kelvin','Luminious intensity':'Candela',
+     'Electric current':'Ampere'}
+b = input('Enter any Fundamental quantity from System international')
+print(a[b])
+
+
+
+
+
+
