975309e9b7ff05aa12410d1ef8daad4ec5182344
Yahya-1254 <79520504+Yahya-1254@users.noreply.github.com>
2021-03-06 19:09:16 +0500

    Add files via upload

---
+++ faulty calculator.py
@@ -0,0 +1,25 @@
+##45*3=555 56+9=77 56/6=4
+print("Enter any number")
+a = int(input())
+print ("Emter another number")
+b = int(input())
+print ('Chose the opreator \n + \n - \n / \n *')
+c = (input())
+if a==45 and b == 3 and c == "*":
+    print('555')
+elif a==56 and b==9 and c=="+":
+    print("77")
+elif a==56 and b==6 and c=="/":
+    print(n1+n2)
+elif c=="+":
+    print(n1+n2)
+elif c=="-":
+    print(n1-n2)
+elif c == "*":
+    print(n1*n2)
+elif c == "/":
+    print(n1/n2)
+else:
+    print("Error")    
+
+
